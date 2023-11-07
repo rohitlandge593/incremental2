76a108a942926c1d6d06f532db48063c8dde95ba
Iamneo <techadmin@iamneo.ai>
2023-11-07 11:45:47 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -3,4 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace dotnetapp.Models
 {
+    public class Player 
+    {
+        public int Id{get;set;}
+        public int Age{get;set;}
+        public string Name{get;set;}
+        public string Category{get;set;}
+        public decimal BiddingPrice{get;set;}
+    }
 }
