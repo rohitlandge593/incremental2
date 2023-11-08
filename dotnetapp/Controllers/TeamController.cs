7e9dec9bc3f37b36c9d99cce4f6b20af2885db57
Iamneo <techadmin@iamneo.ai>
2023-11-08 15:58:32 +0000

    Backup Commit

--- dotnetapp/Controllers/TeamController.cs
+++ dotnetapp/Controllers/TeamController.cs
@@ -18,11 +18,27 @@ namespace dotnetapp.Controllers
         // {
         //     _logger = logger;
         // }
-        public TeamController(Applicatio)
+        ApplicationDbContext _context;
+        public TeamController(ApplicationDbContext context)
+        {
+            _context=context;
+        }
 
         public IActionResult Index()
         {
+            var teamList=_context.Teams;
+            return View(teamList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Team team)
+        {
+            _context.Teams.Add(team);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
