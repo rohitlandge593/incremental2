5d23677c877729a6d0e5be30e047bc84e26c5e5f
Iamneo <techadmin@iamneo.ai>
2023-11-20 07:24:17 +0000

    Backup Commit

--- dotnetapp/Controllers/TeamController.cs
+++ dotnetapp/Controllers/TeamController.cs
@@ -41,11 +41,41 @@ namespace dotnetapp.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var teamList=_context.Teams.FirstOrDefault(i=>i.TeamId==id);
+            if(teamList!=null)
+            {
+                return View(teamList);
+            }
+            else    
+                return NotFound();
+        }
+        [HttpPost]
+        public IActionResult Edit(int id,Team team)
+        {
+            var teamList=_context.Teams.FirstOrDefault(i=>i.TeamId==id);
+            if(teamList!=null)
+            {
+                teamList.TeamId=team.TeamId;
+                teamList.Name=team.Name;
+                _context.SaveChanges();
+               return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error!");
         }
+
     }
 }
